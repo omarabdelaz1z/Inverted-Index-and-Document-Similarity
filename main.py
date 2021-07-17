6ad41b88e35b74c1e2746992a6376840eeb9da9a
omarabdelaz1z <omarabdelaziz042@gmail.com>
2021-07-18 01:35:07 +0200

    Added utility function to print the dictionary as pretty JSON, also added a sample test case for term_info method

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from glob import glob
 from inverted_index import InvertedIndex
+from json import dumps
 
 
 def fetch_content(filename):
@@ -9,14 +10,23 @@ def fetch_content(filename):
         return content
 
 
+def as_json(data, indent=3):
+    return dumps(data, indent=indent)
+
+
 if __name__ == '__main__':
     path = "tmp/*.txt"  # can be changed.
     files = glob(path)
-    documents = []
+    documents = {}
 
-    for file in files:
-        documents.append(fetch_content(file))
+    for filename in files:
+        documents[filename] = fetch_content(filename)
 
     inverted_index = InvertedIndex()
-    inverted_index.build(documents)
-    print(inverted_index.find('different system should results are in cost and can only computing elements'))
+    inverted_index.build_index(documents)
+
+    resultSet = inverted_index.find('different system should results are in cost and can only computing elements')
+    print(as_json(resultSet))
+
+    termInfo = inverted_index.term_info("agile")
+    print(as_json(termInfo))
